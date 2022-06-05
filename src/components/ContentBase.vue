4ff42968faa6cc1f40cdd3869af5bf6ca3983b92
xing gao <2416457672@qq.com>
2022-06-05 11:15:00 +0800

    myspace

---
+++ src/components/ContentBase.vue
@@ -0,0 +1,23 @@
+<template>
+    <div calss="home">
+        <div class="container">
+            <div class="card">
+                <div calss="card-body">
+                    <slot></slot>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ContentBase",
+}
+</script>
+
+<style scoped>
+.container {
+    margin-top: 20px;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,51 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue';
+import UserListView from '../views/UserListView';
+import UserProfileView from '../views/UserProfileView';
+import LoginView from '../views/LoginView';
+import RegisterView from '../views/RegisterView';
+import NotFoundView from '../views/NotFoundView';
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/userlist/',
+    name: 'userlist',
+    component: UserListView
+  },
+  {
+    path: '/userprofile/:userId/',
+    name: 'userprofile',
+    component: UserProfileView
+  },
+  {
+    path: '/login/',
+    name: 'login',
+    component: LoginView
+  },
+  {
+    path: '/register/',
+    name: 'register',
+    component: RegisterView
+  },
+  {
+    path: '/404/',
+    name: '404',
+    component: NotFoundView
+  },
+  {
+    psth: '/:catchAll(.*)',
+    redirext: "/404/"
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
